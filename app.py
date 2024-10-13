0d7095ee5b46bc424b1f2abd44e414e59f17158c
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-12 23:45:52 -0400

    Flask e-Commerce
    -- ALL IN ONE STOP

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB connection
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/products')
+def products():
+    products = products_collection.find()
+    return render_template('products.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
